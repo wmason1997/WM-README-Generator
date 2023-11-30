b1028b1fda8a9475d060ceadb2cd514484d6e8a0
wmason1997 <williamcmason1997@gmail.com>
2023-11-29 20:06:25 -0800

    I added some questions to the index.js file as well as the necessary packages.

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'How would you describe your project?',
+    },
+    {
+        type: 'input',
+        name: 'installationInstructions',
+        message: 'How do you install this project?',
+    },
+    {
+        type: 'input',
+        name: 'usageInstructions',
+        message: 'What are some usage instructions for this project?',
+    },
+    {
+        type: 'input',
+        name: 'contributionGuidelines',
+        message: 'What are the contribution guidelines for this project?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the tests for this project',
+    },
+    {
+        type: 'input',
+        name: 'GitHubProfile',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
